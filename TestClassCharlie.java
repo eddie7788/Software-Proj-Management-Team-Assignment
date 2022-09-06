85a301bc4131891433d2664df27dd85cb2b707d9
charlesblyton <charles.blyton@gmail.com>
2022-09-06 11:57:41 +1000

    First commit from charlie's intellij IDE

--- TestClassCharlie.java
+++ TestClassCharlie.java
@@ -1,5 +1,6 @@
 public class TestClassCharlie {
     public static void main(String[] args) {
         System.out.println("Did you get this file??");
+        System.out.println("Try Committing a file to make sure it work..");
     }
 }
